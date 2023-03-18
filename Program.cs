fbd7564d186930436b067c5c20c4d8e120ab3dbf
Leandro Gonçalves <goncalvesleandro68@gmail.com>
2023-03-18 13:39:21 -0300

    Recursividade add

--- Program.cs
+++ Program.cs
@@ -6,17 +6,23 @@ class Program
     public static void Main(string[] args)
     {
         
-     Repeticao repeticao = new Repeticao();
-         Sequencial sequencial = new Sequencial();
-         Decisao decisao = new Decisao();
-         Funcoes funcoes = new Funcoes();
-         Vetores vetores = new Vetores();
-         Multplicacao mult = new Multplicacao();
-         Media media = new Media();
-         Ordencao ordem = new Ordencao();
-        Console.Clear();
-        ordem.ordem();
+    //  Repeticao repeticao = new Repeticao();
+    //      Sequencial sequencial = new Sequencial();
+    //      Decisao decisao = new Decisao();
+    //      Funcoes funcoes = new Funcoes();
+    //      Vetores vetores = new Vetores();
+    //      Multplicacao mult = new Multplicacao();
+    //      Media media = new Media();
+    //      Ordencao ordem = new Ordencao();
+    //     Console.Clear();
+    //     ordem.ordem();
+
+    int n1,n2=0;
+    Console.WriteLine("Informe o numero para saber a tabuada:");
+    n1 = Convert.ToInt32(Console.ReadLine());
+    Tabuada.tabuada(n1,n2);
         
+
 
 }
 }
---
+++ Recursividade/Tabuada.cs
@@ -0,0 +1,14 @@
+class Tabuada{
+
+    public static void tabuada(int n1, int n2){
+
+        if(n2 > 10){
+            return;
+        }
+        Console.WriteLine($"{n1} x {n2} = {(n1*n2)}");
+        tabuada(n1,n2+1);
+        n2++;
+
+    }
+
+}
