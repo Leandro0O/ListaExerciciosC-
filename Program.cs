7eb5eddc265ac505bc8f682049778632871c30d4
Leandro Gonçalves <goncalvesleandro68@gmail.com>
2023-03-20 11:59:19 -0300

    recursividade

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     public static void Main(string[] args)
     {
         
-     Repeticao repeticao = new Repeticao();
+        Repeticao repeticao = new Repeticao();
          Sequencial sequencial = new Sequencial();
          Decisao decisao = new Decisao();
          Funcoes funcoes = new Funcoes();
@@ -16,10 +16,13 @@ class Program
          Ordencao ordem = new Ordencao();
         Console.Clear();
 
-    int n1,n2=0;
-    Console.WriteLine("Informe o numero para saber a tabuada:");
-    n1 = Convert.ToInt32(Console.ReadLine());
-    Tabuada.tabuada(n1,n2);
+        int n1,n2=0;
+        Console.WriteLine("Informe o numero para saber a tabuada:");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        Recursividade.tabuada(n1,n2);
+
+
+
 
     }
     
--- Recursividades/Recursividade.cs
+++ Recursividades/Recursividade.cs
@@ -1,4 +1,6 @@
-class Tabuada{
+class Recursividade{
+
+
     public static void tabuada(int n1, int n2){
 
         if(n2 > 10){
@@ -7,5 +9,14 @@ class Tabuada{
         Console.WriteLine($"{n1} x {n2} = {(n1*n2)}");
         tabuada(n1,n2+1);
     }
+
+    public static int soma(int n1, int n2){
+        int res = 0;
+        if(res < n2){
+            res = n1 + soma(n1,n2-1);
+        }
+        return res;
+    }
+
 
 }
