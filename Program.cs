fc61455e611d16cf8dc1446d1b8e155b1b0b080a
Leandro Gonçalves <goncalvesleandro68@gmail.com>
2023-03-21 18:07:12 -0300

    att

--- Program.cs
+++ Program.cs
@@ -2,10 +2,7 @@
 
 class Program
 {
-
-    public static void Main(string[] args)
-    {
-        
+    public static void Main(string[] args){      
         Repeticao repeticao = new Repeticao();
          Sequencial sequencial = new Sequencial();
          Decisao decisao = new Decisao();
@@ -15,19 +12,13 @@ class Program
          Media media = new Media();
          Ordencao ordem = new Ordencao();
         Console.Clear();
-
         int n1,n2=0;
         Console.WriteLine("Informe o numero para saber a tabuada:");
         n1 = Convert.ToInt32(Console.ReadLine());
         Recursividade.tabuada(n1,n2);
 
-
-
-
     }
-    
-
-
+  
 }
 
 
